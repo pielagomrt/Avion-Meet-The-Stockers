dba4d67f1447f90aa9ecdc32f1afd68b2dfcdd27
Rio <rjmssevilla@gmail.com>
2021-03-02 23:32:38 +0400

    added devise with 3 models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :buyers
+  devise_for :brokers
+  devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
