f35005852d4e8b32fedbba87fcdb419785503341
Rio <rjmssevilla@gmail.com>
2021-03-03 00:41:44 +0400

    added-index-root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :buyers
   devise_for :brokers
   devise_for :admins
+
+  root to: "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
