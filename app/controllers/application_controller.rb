80748b7f5bf2636c1b1a7da67db021d7ee842adf
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-03-03 23:51:43 +0300

    add views to admin, broker, buyer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password)}
   end
+
+  def after_sign_in_path_for(resource)
+    case resource
+       when Admin then admins_path
+       when Broker then broker_path(@broker)
+       when Buyer then buyer_path(@buyer)
+    end
+  end
 end
