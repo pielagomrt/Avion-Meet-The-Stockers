b5963e71fbb35b12febd0ac7ca1d52179eb12148
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-03-05 15:24:43 +0300

    add create, edit, view to admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -4,4 +4,21 @@ class AdminsController < ApplicationController
         @brokers = Broker.all
         @buyers = Buyer.all
     end
+
+    def new_broker
+        @broker = Broker.new
+    end
+
+    def new_buyer
+        @buyer = Buyer.new
+    end
+
+    def edit_broker
+        @broker = Broker.find(params[:id])
+    end
+
+    def edit_buyer
+        @buyer = Buyer.find(params[:id])
+    end
+
 end
--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,4 +2,16 @@ class BuyersController < ApplicationController
     def show
         @buyer = Buyer.find(params[:id])
     end
+
+    def update
+        @buyer = Buyer.find(params[:id])
+        @buyer.update(buyer_params)
+        redirect_to admins_path
+    end
+
+    private
+
+    def buyer_params
+        params.require(:buyer).permit(:first_name, :last_name, :email)
+    end
 end
