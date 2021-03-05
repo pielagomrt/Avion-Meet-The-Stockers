257fb8cde6698fee8789e18d0fc1f045bd0fccca
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-03-05 17:24:37 +0300

    corrects code alignment

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,24 +1,23 @@
 class AdminsController < ApplicationController
 
     def index
-        @brokers = Broker.all
-        @buyers = Buyer.all
+      @brokers = Broker.all
+      @buyers = Buyer.all
     end
 
     def new_broker
-        @broker = Broker.new
+      @broker = Broker.new
     end
 
     def new_buyer
-        @buyer = Buyer.new
+      @buyer = Buyer.new
     end
 
     def edit_broker
-        @broker = Broker.find(params[:id])
+      @broker = Broker.find(params[:id])
     end
 
     def edit_buyer
-        @buyer = Buyer.find(params[:id])
+      @buyer = Buyer.find(params[:id])
     end
-
 end
