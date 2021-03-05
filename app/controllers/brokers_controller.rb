3f3dc982ba7eac98978cf22b931bbe687bba4e96
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-03-05 18:00:41 +0300

    corrects code alignment

--- app/controllers/brokers_controller.rb
+++ app/controllers/brokers_controller.rb
@@ -1,17 +1,17 @@
 class BrokersController < ApplicationController
-    def show
-        @broker = Broker.find(params[:id])
-    end
+  def show
+    @broker = Broker.find(params[:id])
+  end
 
-    def update
-        @broker = Broker.find(params[:id])
-        @broker.update(broker_params)
-        redirect_to admins_path
-    end
+  def update
+    @broker = Broker.find(params[:id])
+    @broker.update(broker_params)
+    redirect_to admins_path
+  end
 
-    private
+  private
 
-    def broker_params
-        params.require(:broker).permit(:first_name, :last_name, :email)
+  def broker_params
+    params.require(:broker).permit(:first_name, :last_name, :email)
     end
 end
--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,17 +1,17 @@
 class BuyersController < ApplicationController
-    def show
-        @buyer = Buyer.find(params[:id])
-    end
+  def show
+    @buyer = Buyer.find(params[:id])
+  end
 
-    def update
-        @buyer = Buyer.find(params[:id])
-        @buyer.update(buyer_params)
-        redirect_to admins_path
-    end
+  def update
+    @buyer = Buyer.find(params[:id])
+    @buyer.update(buyer_params)
+    redirect_to admins_path
+  end
 
-    private
+  private
 
-    def buyer_params
-        params.require(:buyer).permit(:first_name, :last_name, :email)
-    end
+  def buyer_params
+    params.require(:buyer).permit(:first_name, :last_name, :email)
+  end
 end
