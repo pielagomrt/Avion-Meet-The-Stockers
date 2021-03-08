ece725167bbac8eeeb49fe91a65d496a49204f90
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-03-08 21:57:54 +0300

    fixes code alignment

--- app/mailers/broker_mailer.rb
+++ app/mailers/broker_mailer.rb
@@ -1,14 +1,14 @@
 class BrokerMailer < ApplicationMailer
-    default from: 'from@example.com'
-    layout 'mailer'
+  default from: 'from@example.com'
+  layout 'mailer'
 
-    def new_broker_pending(email)
-      @email = email
-      mail(to: @email, subject: 'Account pending for approval')
-    end
+  def new_broker_pending(email)
+    @email = email
+    mail(to: @email, subject: 'Account pending for approval')
+  end
 
-    def new_broker_approved(email)
-      @email = email
-      mail(to: @email, subject: 'Account approved')
-    end
+  def new_broker_approved(email)
+    @email = email
+    mail(to: @email, subject: 'Account approved')
+  end
 end
