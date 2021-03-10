2396e9dc6e3856a7cdf247c49e56205fb62fe3a1
REGINA PIELAGO <pielago.mrt@gmail.com>
2021-03-10 21:15:45 +0300

    added mailer test for broker_mailer

--- spec/mailers/broker_mailer_spec.rb
+++ spec/mailers/broker_mailer_spec.rb
@@ -1,5 +1,33 @@
 require "rails_helper"
 
 RSpec.describe BrokerMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:broker) {create(:broker)}
+
+  describe "1. new broker pending approval" do
+    let(:mail) {BrokerMailer.new_broker_pending(broker.email)}
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Account pending for approval")
+      expect(mail.to).to eq([broker.email])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to have_content("registered")
+    end
+  end
+
+  describe "2. new broker approved" do
+    let(:mail) {BrokerMailer.new_broker_approved(broker.email)}
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Account approved")
+      expect(mail.to).to eq([broker.email])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to have_content("approved")
+    end
+  end
 end
