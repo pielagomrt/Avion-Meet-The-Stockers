2ed30f020f47c9a458109de7e232af3205b6c20f
Rio <rjmssevilla@gmail.com>
2021-03-16 21:17:36 +0400

    added transaction and portfolio

--- app/controllers/buyer_stocks_controller.rb
+++ app/controllers/buyer_stocks_controller.rb
@@ -5,11 +5,14 @@ class BuyerStocksController < ApplicationController
     end
 
     def create
-      @broker_stock = BrokerStock.find(params[:id])
-      current_buyer.broker_stocks << @broker_stock
-
-      if current_buyer.save
+      @buyer_stock = BuyerStock.new
+      @buyer_stock.broker_stock_id = BrokerStock.find(params[:id]).id
+      @buyer_stock.buyer_id = current_buyer.id
+      @buyer_stock.quantity = params[:buyer_stock][:quantity]
+      if @buyer_stock.save
         redirect_to buyer_portfolio_path(current_buyer.id)
+      else
+        flash[:notice] = "Quantity must be greater than 0"
       end
     end
 
--- app/models/buyer_stock.rb
+++ app/models/buyer_stock.rb
@@ -1,4 +1,8 @@
 class BuyerStock < ApplicationRecord
+    validates :quantity, numericality: { greater_than: 0,  only_integer: true }
     belongs_to :broker_stock
     belongs_to :buyer
+
+    
+    # buyer_stock.broker_stock.stock.price.to_f * buyer_stock.quantity
 end
